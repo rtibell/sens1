b34d27cabdce5f8c1a4c00e7fd1e327f0cbcbfb0
Rasmus Tibell <rasmus.tibell@gmail.com>
2016-06-16 07:56:26 +0200

    upd

--- Display.py
+++ Display.py
@@ -2,15 +2,37 @@ from sense_hat import SenseHat
 from threading import Thread
 from multiprocessing import Queue
 
+BLK = [0, 0, 0]  # Black
+RED = [255, 0, 0]  # Red
+GRE = [0, 255, 0]   # Green
+BLU = [0, 0, 255]   # Blue
+WHI = [255, 255, 255]  # White
+
 class Display(Thread):
     def __init__(self, prod):
         Thread.__init__(self)
         self.daemon = True
         self.prod = prod
         self.dorun = True
+        self.Sense = SenseHat()
+        self.Sense.clear()
+        Self.DSPbuff = [
+                        RED, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK,
+                        BLK, BLK, BLK, BLK, BLK, BLK, BLK, WHI
+                        ]
+    
+    def dsp(self):
+        self.Sense.set_pixels(self.DSPbuff)
         
     def run(self):
         print("Running Cons")
+        dsp()
         while (self.dorun):
             print('{} {}'.format('Cons', self.prod.getNext()))
         print("Stopping Cons")
