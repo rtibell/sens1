b62b2b09fbd8c65add14dfb499b365f1b1b6ae3c
rtibell <rasmus.tibell@gmail.com>
2016-06-23 22:44:03 +0200

    upd

--- Accelerometer.py
+++ Accelerometer.py
@@ -38,7 +38,6 @@ class Accelerometer(Thread):
         self.iters = int(self.quantum / self.period)
         self.que = Queue(1024)
         self.dorun = True
-        self.acc_offset = 0
         self.acc_bias = [0.0, 0.0, 0.0]
         self.Rmtx = IDmtx
         self.bin_logger = bin_logger
@@ -74,39 +73,6 @@ class Accelerometer(Thread):
 #                          acc_dic, ruck_dic, acc_list[0], cc_list[-1]])
             
 
-    def old_run(self):
-        print("Calibrating...")
-        self.adjust()
-        print("Calibration done!")
-        while (self.dorun):
-            i = self.iters
-            max = -1000000000.0
-            min = 1000000000.0
-            sum = 0.0
-            acc_first = self.read_acc()
-            while (i > 0):
-                rd = self.read_acc()
-                av = self.get_adj_len()
-                sum = sum + av
-                if (max < av):
-                    max = av
-                    acc_max = rd
-                    acc_max_i = (self.iters - i)
-                if (min > av):
-                    min = av
-                    acc_min = rd 
-                    acc_min_i = (self.iters - i)
-                i = i - 1
-                time.sleep
-            acc_last = self.read_acc()
-            ruck_avg = sqrt(pow(acc_first['x']-acc_last['x'], 2)+pow(acc_first['y']-acc_last['y'], 2)+pow(acc_first['z']-acc_last['z'], 2))/self.quantum
-            ruck_max = sqrt(pow(acc_max['x']-acc_min['x'], 2)+pow(acc_max['y']-acc_min['y'], 2)+pow(acc_max['z']-acc_min['z'], 2))/(abs(acc_min_i-acc_max_i)*self.period)
-            acc_dic = {'avg': (sum/float(self.iters)), 'min': min, 'max': max}
-            ruck_dic = {'avg': ruck_avg, 'max': ruck_max}
-            self.que.put([dt.datetime.now().strftime('%Y%m%d %H:%M.%S.%f'), 
-                          acc_dic, ruck_dic, acc_first, acc_last])
-            time.sleep(self.period)
-
     def writeBinLog(self, entry):
         if (self.bin_logger == None):
             return
@@ -123,41 +89,21 @@ class Accelerometer(Thread):
             x += rd['x']
             y += rd['y']
             z += rd['z']
-            av = self.get_len()
-            ac += av
             time.sleep(0.001)
-        self.acc_offset = ac/100.0
-        self.acc_offset *= 1.02
         self.acc_bias = [x/100.0, y/100.0, z/100.0]
         
 
     def read_acc(self):
         a1 = self.Sense.get_accelerometer_raw()
         self.writeBinLog(a1)
-        self.acc = a1
         return a1
 
     def get(self):
         return [self.get_x(), self.get_y(), self.get_z()] 
 
-    def get_x(self):
-        return self.acc['x']
-
-    def get_y(self):
-        return self.acc['y']
-
-    def get_z(self):
-        return self.acc['z']
-
     def calc_len(self, acc):
         return sqrt(pow(acc[0], 2) + pow(acc[1], 2) + pow(acc[2], 2))
 
-    def get_len(self):
-        self.Length = sqrt(pow(self.get_x(),2) + pow(self.get_y(),2) + pow(self.get_z(),2))
-        return self.Length
-
-    def get_adj_len(self):
-        return self.get_len()-self.acc_offset
 
     def adjAcc(self, acc):
         return [acc[0]-self.acc_bias[0], acc[1]-self.acc_bias[1], acc[0]+(Gdelta-self.acc_bias[0])]
