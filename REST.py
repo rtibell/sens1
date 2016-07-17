27924edf176479bc8cce7d3c5425e0dc859be70f
Rasmus Tibell <rasmus.tibell@gmail.com>
2016-07-17 20:46:49 +0200

    upd

--- REST.py
+++ REST.py
@@ -2,50 +2,60 @@ import web
 import json
 from multiprocessing import Queue
 from threading import Thread
+import math
 
-ring_buffer = []
-buffer_idx = 0
+BUFF_SIZE = 50
+buffer = [None] * BUFF_SIZE
+idx = 0
+global buffer, idx
 
 class RESTThread(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.dorun = True
-        self.que = Queue(1024)
         self.urls = ('/', 'HelloWorld')
-        self.app = web.application(self.urls, globals(), autoreload=True)
+        globs = globals()
+        globs.update({'RESTself':self})
+        self.app = web.application(self.urls, globs, autoreload=True)
+        global buffer, idx
         
     def run(self):
-        print("Start REST Server")
         while (self.dorun):
             self.app.run()
-            print("REST app started")
-            entry = self.getNext()
-            print("Entry:")
-            print(entry)
         self.app.stop()
 
     def stopit(self):
-        self.dorun = False
-        
-    def getNext(self):
-        try:
-            return self.que.get(True, timeout=600)    
-        except:
-            print("Exception")
-
-    def read_queue(self):
-        yield self.que.get(True, None)
+        self.dorun = False    
         
     def putNext(self, entry):
-        self.que.put_nowait(entry)
+        global buffer, idx
+        buffer[idx] = entry
+        idx = (idx+1) % BUFF_SIZE
+        print(idx)
+        
     
     
 class HelloWorld:
+       
     def GET(self):
-        return json.dumps({"labels": ["January", "February", "March", "April", "May", "June", "July"],
-                "series": ["Series A", "Series B"],
-                "data": [[65, 18, 80, 81, 56, 55, 40],
-                         [28, 29, 40, 19, 86, 27, 90]]})
+        global buffer, idx
+        retbuff = buffer[idx:-1] + buffer[0:idx] 
+        #print(retbuff)
+        labels = map(lambda x: x[0][9:], retbuff)
+        series = ["Acc max", "Acc avg", "Acc min"]
+        datamax = map(lambda x: x[1]['max'], retbuff)
+        dataavg = map(lambda x: x[1]['avg'], retbuff)
+        datamin = map(lambda x: x[1]['min'], retbuff)
+        print(" max={}\n avg={}\n min={}".format(datamax, dataavg, datamin))
+        #return json.dumps({"labels": ["January", "February", "March", "April", "May", "June", "July"],
+        #        "series": ["Series A", "Series B"],
+        #        "data": [[65, 18, 80, 81, 56, 55, 40],
+        #                 [28, 29, 40, 19, 86, 27, 90]]})
+        return json.dumps({"labels": labels,
+                "series": series,
+                "data": [datamax,
+                         dataavg,
+                         datamin]})
 
 
--- push.sh
+++ push.sh
@@ -2,5 +2,6 @@
 
 git status
 git add Accelerometer.py Runner.py Display.py DBController.py push.sh FileController.py BinaryFileController.py
+git add REST.py html/chart.js html/graph.html
 git commit -m 'upd'
 git push origin master
