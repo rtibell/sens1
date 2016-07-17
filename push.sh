f2abbc7f2d0da495cf33d2a61315ea2c32657683
Rasmus Tibell <rasmus.tibell@gmail.com>
2016-07-17 20:47:24 +0200

    upd

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm *.pyc
 git status
 git add Accelerometer.py Runner.py Display.py DBController.py push.sh FileController.py BinaryFileController.py
 git add REST.py html/chart.js html/graph.html
